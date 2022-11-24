66b2ea0b0005041c4b8e54f01e9ed268cf56a743
Carl Madsen <madsencarlb@gmail.com>
2022-11-25 00:25:16 +1100

    added error handling to getWeather Promises

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,10 +1,28 @@
 const apiKey = 'd65152ea28ed7bb6aa6f34676f08bc27';
 
+function checkResponse(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    } else {
+        return response;
+    }
+}
+
 async function getGeocodeData(city) {
-    let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`,
-        { mode: "cors" })
-    let geoData = await response.json();
-    return geoData[0];
+    try {
+        let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`,
+            { mode: "cors" })
+        checkResponse(response);
+        let geoData = await response.json();
+        if (!geoData[0]) {
+            throw Error("Could not find city");
+        } else {
+            return geoData[0];
+        }
+    }
+    catch (err) {
+        throw err;
+    }
 }
 
 async function getWeatherData(location, units) {
@@ -12,6 +30,7 @@ async function getWeatherData(location, units) {
     let lat = location.lat;
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`,
         { mode: "cors" });
+        checkResponse(response);
     let weatherData = await response.json();
     return weatherData;
 }
@@ -20,7 +39,7 @@ async function getWeather(location, units) {
     let geoData = getGeocodeData(location);
     let weatherData = geoData.then(loc => {
         return getWeatherData(loc, units);
-    });
+    })
     Promise.all([geoData, weatherData])
         .then(([geoData, weatherData]) => {
             console.log(`got geo data:`)
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const apiKey = 'd65152ea28ed7bb6aa6f34676f08bc27';
 let units = "metric";
 
 // convert the city into the correct format before passing it to getWeather:
-    //
-    //
+//
+//
 getWeather("Melbourne");
